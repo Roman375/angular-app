10fe19c7526e0668c43f89804c9b231c37abc6ef
Roman375 <romanlviv96@gmail.com>
2021-02-25 20:21:52 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'task-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task-app');
+    // expect(app.title).toEqual('task-app');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task-app';
+  search = "";
+  products;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<SearchResults>('http://demo3062546.mockable.io/products')
+      .subscribe({
+        next: (data) => {
+          this.products = data.products;
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+        },
+      });
+  }
+}
+
+interface SearchResults {
+  count_products: number;
+  page_count: number;
+  products: Array<object>;
+  page: number
 }
